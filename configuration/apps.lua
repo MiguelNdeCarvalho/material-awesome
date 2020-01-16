c7a0111a0b8444d3a47c2aff9ff7f7a1ff788a63
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2020-01-16 13:44:46 +0000

    start-up: Add mailspring to start minimized by default

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -42,7 +42,8 @@ return {
      --'synology-drive -minimized',
      'steam -silent',
      'discord --start-minimized',
-     'slack -u'
+     'slack -u',
+     'mailspring -b',
     -- Add applications that need to be killed between reloads
     -- to avoid multipled instances, inside the awspawn script
     '~/.config/awesome/configuration/awspawn' -- Spawn "dirty" apps that can linger between sessions
